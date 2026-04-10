75bd158ad2dc5e6ff7e72763ea82426d5d12d1ea
M Zharfan Wiranata <zhargorist10@gmail.com>
2026-04-10 20:30:08 +0800

    update day6

--- [day-06]_four_bit_ripple_carry_adder/four_bit_ripple_carry_adder_tb.v
+++ [day-06]_four_bit_ripple_carry_adder/four_bit_ripple_carry_adder_tb.v
@@ -1,8 +1,14 @@
 module four_bit_ripple_carry_adder_tb();
 reg [3:0]a,b;
-wire [3:0]s,c;
+wire [3:0]s;
 wire cout;
-four_bit_ripple_carry_adder uut(.a(a),.b(b),.s(s),.c(c),.cout(cout));
+four_bit_ripple_carry_adder uut(.a(a),.b(b),.s(s),.cout(cout));
+
+initial begin
+$dumpfile("four_bit_ripple_carry_adder_tb.vcd");
+$dumpvars(0, four_bit_ripple_carry_adder_tb);
+end
+
 initial begin
 a=4'b0000;b=4'b0110;#100;
 a=4'b0011;b=4'b1001;#100;
