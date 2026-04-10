278d342211641f569802efd656eb189db04f8bd7
M Zharfan Wiranata <zhargorist10@gmail.com>
2026-04-10 14:59:26 +0800

    rapikan folder lagi

--- [day-01]_basic_gates/basic_gates_tb.v
+++ [day-01]_basic_gates/basic_gates_tb.v
@@ -1,7 +1,15 @@
+`timescale 1ns/1ps
+
 module basic_gates_tb();
 reg A,B,C;
 wire P,Q,R,S,T,U;
 basic_gates uut(.A(A),.B(B),.C(C),.P(P),.Q(Q),.R(R),.S(S),.T(T),.U(U));
+
+initial begin
+    $dumpfile("basic_gates_tb.vcd");
+    $dumpvars(0, basic_gates_tb);
+end
+
 initial begin 
 A=0;B=0;C=0; #100;
 A=0;B=0;C=1; #100;
